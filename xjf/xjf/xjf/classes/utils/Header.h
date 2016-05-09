f074a370cc3c2270e871e799180efe487a74de2b
Hunter_Wang <haihaiteng@sina.com>
2016-05-09 10:32:12 +0800

    新增 Header.h 常用的类库.h文件都在这里。 新增PrefixHeader.pch  将Header.h 导入pct 方便全局使用。

---
+++ xjf/xjf/xjf/classes/utils/Header.h
@@ -0,0 +1,30 @@
+//
+//  Header.h
+//  xjf
+//
+//  Created by Hunter_wang on 16/5/9.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+//thirds
+#import <AFNetworking.h>
+#import <MJRefresh.h>
+#import "MBProgressHUD.h"
+#import <Masonry.h>
+#import <UIImageView+WebCache.h>
+
+//category
+#import "UIColor+hexColor.h"
+#import "ZToastManager.h"
+#import "UILabel+StringFrame.h"
+#import "NSString+Extensions.h"
+
+//
+#import "xjfConfigure.h"
+#import "XjfShare.h"
+#import "XjfRequest"
+
+#endif /* Header_h */
