7c1c5a838490656dc2bfdda5e4721151400188e7
PerryJi <jiguanhu@xijinfa.com>
2016-05-16 11:36:25 +0800

    add QQ login

--- xjf/xjf/xjf/AppDelegate.m
+++ xjf/xjf/xjf/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 #import "RootViewController.h"
-
+#import <TencentOpenApiSDK/TencentOpenAPI/TencentOAuth.h>
 @interface AppDelegate ()
 
 @end
@@ -25,7 +25,12 @@
 
     return YES;
 }
-
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
+    return [TencentOAuth HandleOpenURL:url];
+}
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
+    return [TencentOAuth HandleOpenURL:url];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
