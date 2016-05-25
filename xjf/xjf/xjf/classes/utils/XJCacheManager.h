04ccacee9a98141cd8fe66884a7170d388a64e2a
PerryJi <jiguanhu@xijinfa.com>
2016-05-25 09:43:07 +0800

    update image asset

---
+++ xjf/xjf/xjf/classes/utils/XJCacheManager.h
@@ -0,0 +1,13 @@
+//
+//  XJCacheManager.h
+//  xjf
+//
+//  Created by PerryJ on 16/5/24.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XJCacheManager : NSObject
+
+@end
---
+++ xjf/xjf/xjf/classes/utils/XJCacheManager.m
@@ -0,0 +1,13 @@
+//
+//  XJCacheManager.m
+//  xjf
+//
+//  Created by PerryJ on 16/5/24.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#import "XJCacheManager.h"
+
+@implementation XJCacheManager
+
+@end
