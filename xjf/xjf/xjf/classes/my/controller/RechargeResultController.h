1abc54de91bc88c94302a915babc57150dcc3bd8
PerryJi <jiguanhu@xijinfa.com>
2016-07-04 17:30:48 +0800

    Build RechargeResultVC By XIB

---
+++ xjf/xjf/xjf/classes/my/controller/RechargeResultController.h
@@ -0,0 +1,13 @@
+//
+//  RechargeResultController.h
+//  xjf
+//
+//  Created by PerryJ on 16/7/4.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface RechargeResultController : BaseViewController
+
+@end
