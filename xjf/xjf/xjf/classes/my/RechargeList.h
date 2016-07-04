b79b1ca1666f3f888e413792738685aedc4387d4
PerryJi <jiguanhu@xijinfa.com>
2016-07-04 14:48:50 +0800

    Add RechargeList Model

---
+++ xjf/xjf/xjf/classes/my/RechargeList.h
@@ -0,0 +1,26 @@
+//
+//  RechargeList.h
+//  xjf
+//
+//  Created by PerryJ on 16/7/4.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+@protocol RechargeDeal <NSObject>
+
+@end
+@interface RechargeDeal : JSONModel
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, assign) NSInteger amount;
+@property (nonatomic, assign) NSInteger bonus;
+@end
+@interface RechargeResult : JSONModel
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, strong) NSMutableArray <RechargeDeal>*deal;
+@end
+@interface RechargeList : JSONModel
+@property (nonatomic, assign) NSInteger errCode;
+@property (nonatomic, copy) NSString *errMsg;
+@property (nonatomic, strong) RechargeResult *result;
+@end
---
+++ xjf/xjf/xjf/classes/my/RechargeList.m
@@ -0,0 +1,27 @@
+//
+//  RechargeList.m
+//  xjf
+//
+//  Created by PerryJ on 16/7/4.
+//  Copyright © 2016年 lcb. All rights reserved.
+//
+
+#import "RechargeList.h"
+
+@implementation RechargeDeal
++(BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+@end
+
+@implementation RechargeResult
++(BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+@end
+
+@implementation RechargeList
++(BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+@end
