ec9f8ba09664bbac83197f770edcdcc5418db07a
ggg <guoshiyuan@xijinfa.com>
2016-07-09 13:17:31 +0800

    add OptionalJSONModel

---
+++ xjf/xjf/xjf/OptionalJSONModel.h
@@ -0,0 +1,13 @@
+//
+//  OptionalJSONModel.h
+//  xjf
+//
+//  Created by steve on 7/9/16.
+//  Copyright © 2016 lcb. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface OptionalJSONModel : JSONModel
+
+@end
---
+++ xjf/xjf/xjf/OptionalJSONModel.m
@@ -0,0 +1,13 @@
+//
+//  OptionalJSONModel.m
+//  xjf
+//
+//  Created by steve on 7/9/16.
+//  Copyright © 2016 lcb. All rights reserved.
+//
+
+#import "OptionalJSONModel.h"
+
+@implementation OptionalJSONModel
++ (BOOL)propertyIsOptional:(NSString *)propertyName { return YES; }
+@end
